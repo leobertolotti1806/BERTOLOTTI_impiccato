1c1c3475b18f2591b81b2965d1fb1c5925021c7b
leobertolotti1806 <120721601+leobertolotti1806@users.noreply.github.com>
2023-07-31 22:08:54 +0200

    aggiunta animazione errore

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ let impiccato = document.querySelectorAll("#table > div:first-child > div");
 let tentativiDiv = document.getElementById("tentCount");
 let erroriDiv = document.getElementById("errCount");
 let menu = document.getElementById("menu");
+let errWarning = document.querySelector("body > #circle");
 
 let inserite = [];
 
@@ -38,70 +39,6 @@ for (let i = 0; i < p.length; i++)
     parola.innerHTML += "<div class='card'>_</div>";
 
 console.log("parola => " + p);
-
-document.forms[0].addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let l = lett.value.toLowerCase();
-    lett.value = "";
-
-    if (l != "") {
-        if (!inserite.includes(l)) {
-            /* nuova lettera */
-            inserite.push(l);
-            tentativi++;
-            console.log(inserite);
-
-            if (p.includes(l)) {
-                /* c'è nella parola */
-                let cont = 0;
-                for (let i = 0; i < p.length; i++) {
-                    if (p[i] == l) {
-                        parola.children[i].textContent = l.toUpperCase();
-                        cont++;
-                    } else if (parola.children[i].textContent != "_") {
-                        cont++;
-                    }
-                }
-
-                if (cont == p.length)
-                    showMenu("Hai vinto con " + tentativi + " tentativi!");
-
-            } else {
-                /* non c'è nella parola */
-                bandite.innerHTML += "<div class='card'>" + l + "</div>";
-
-                impiccato[errori].style.opacity = "1";
-
-                errori++;
-
-                erroriDiv.classList.add("animation");
-                erroriDiv.nextElementSibling.style.width = ((errori / 8) * 100) + "%";
-
-                if (errori > 5)
-                    erroriDiv.classList.add("pulse");
-
-                setTimeout(() => { erroriDiv.textContent = errori + " Errori"; }, 500);
-
-                setTimeout(() => { erroriDiv.classList.remove("animation"); }, 1000);
-
-                if (errori == 8)
-                    showMenu("Hai perso con " + errori + " errori!<br>La parola era " + p);
-            }
-
-            tentativiDiv.className = "animation";
-
-            setTimeout(() => {
-                tentativiDiv.textContent = tentativi + " Tentativi";
-            }, 500);
-
-            setTimeout(() => {
-                tentativiDiv.className = "";
-            }, 1000);
-
-        } else alert("Lettera già inserita!");
-    } else alert("Inserisci una lettera!");
-});
 
 function showMenu(msg) {
     lett.disabled = true;
@@ -149,3 +86,81 @@ function showMenu(msg) {
             close();
     };
 }
+
+function reset(time) {
+    errWarning.style.animation = "";
+    lett.disabled = false;
+    lett.focus();
+    lett.nextElementSibling.disabled = false;
+}
+
+document.forms[0].addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let l = lett.value.toLowerCase();
+    lett.value = "";
+
+    if (l != "") {
+        if (!inserite.includes(l)) {
+            /* nuova lettera */
+            inserite.push(l);
+            tentativi++;
+
+            lett.disabled = true;
+            lett.nextElementSibling.disabled = true;
+
+            if (p.includes(l)) {
+                /* c'è nella parola */
+                let cont = 0;
+                for (let i = 0; i < p.length; i++) {
+                    if (p[i] == l) {
+                        parola.children[i].textContent = l.toUpperCase();
+                        cont++;
+                    } else if (parola.children[i].textContent != "_") {
+                        cont++;
+                    }
+                }
+
+                if (cont != p.length)
+                    reset(700);
+                else showMenu("Hai vinto con " + tentativi + " tentativi!");
+
+            } else {
+                /* non c'è nella parola */
+                bandite.innerHTML += "<div class='card'>" + l + "</div>";
+
+                errWarning.style.animation = "pass 2.5s ease";
+
+                reset(2300);
+
+                impiccato[errori].style.opacity = "1";
+
+                errori++;
+
+                erroriDiv.classList.add("animation");
+                erroriDiv.nextElementSibling.style.width = ((errori / 8) * 100) + "%";
+
+                if (errori > 5)
+                    erroriDiv.classList.add("pulse");
+
+                setTimeout(() => { erroriDiv.textContent = errori + " Errori"; }, 500);
+
+                setTimeout(() => { erroriDiv.classList.remove("animation"); }, 1000);
+
+                if (errori == 8)
+                    showMenu("Hai perso con " + errori + " errori!<br>La parola era " + p);
+            }
+
+            tentativiDiv.className = "animation";
+
+            setTimeout(() => {
+                tentativiDiv.textContent = tentativi + " Tentativi";
+            }, 500);
+
+            setTimeout(() => {
+                tentativiDiv.className = "";
+            }, 1000);
+
+        } else alert("Lettera già inserita!");
+    } else alert("Inserisci una lettera!");
+});
